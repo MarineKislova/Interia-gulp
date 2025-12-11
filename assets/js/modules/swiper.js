ed5b3733627a9f7301bc5ac5a6e440d210518a84
Marine <marinekislova@gmail.com>
2025-12-11 17:21:49 +0200

    swiper section 8

--- assets/js/modules/swiper.js
+++ assets/js/modules/swiper.js
@@ -4,7 +4,7 @@ export const swiperHeader = new Swiper('[data-swiper="headerSwiper"]', {
   loop: true,
   slidesPerView: 1,
   speed: 1000,
-  simulateTouch: true,
+  simulateTouch: false,
   centeredSlides: false,
   spaceBetween: 20,
   roundLengths: true,
@@ -18,12 +18,37 @@ export const swiperHeader = new Swiper('[data-swiper="headerSwiper"]', {
   },
 
   // Autoplay
-  // autoplay: {
-  //   delay: 2500,
-  //   disableOnInteraction: false,
-  //   stopOnLastSlide: false,
-  // },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+    stopOnLastSlide: false,
+  },
 
-  // speed: 2500,
+  speed: 4000,
 });
 
+// Header slider
+export const swiperSectionEight = new Swiper('[data-swiper="sectionEightSwiper"]', {
+ loop: true,
+    simulateTouch: true,
+    slidesPerView: 4,
+    spaceBetween: 10,
+    slidesPerGroup: 4,
+
+    breakpoints: {
+      320: { slidesPerView: 1, spaceBetween: 10, slidesPerGroup: 1 },
+      425: { slidesPerView: 2, spaceBetween: 10, slidesPerGroup: 2 },
+      768: { slidesPerView: 3, spaceBetween: 10, slidesPerGroup: 2 },
+      1024: { slidesPerView: 4, spaceBetween: 10, slidesPerGroup: 4 },
+    },
+
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
